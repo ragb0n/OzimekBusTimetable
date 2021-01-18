4c05c7d9315ae5d275f5adb8c6e812941222bfb0
ragb0n <74256820+ragb0n@users.noreply.github.com>
2021-01-18 23:14:24 +0100

    - added departure time calculating algorithm for routes - changed logo

--- timetable/views.py
+++ timetable/views.py
@@ -6,13 +6,13 @@ import datetime
 from .models import *
 
 def home(request):
+    selected_route_id = 2
     routes = Route.objects.all()
-    details = Route_details.objects.filter(route_id=1)
+    details = Route_details.objects.filter(route_id=selected_route_id)
     time_diffs = details.values_list("time_diff", flat=True)
-    starting_hours = Schedule.objects.values_list('start_time', flat=True).filter(route_id = 1)
+    starting_hours = Schedule.objects.values_list('start_time', flat=True).filter(route_id = selected_route_id)
 
     time_list = []
-    counter = 0
     for hour in starting_hours:
         last_stop_time = hour
         time_list_2 = []
@@ -21,10 +21,8 @@ def home(request):
                 last_stop_time = datetime.time(last_stop_time.hour + 1, last_stop_time.minute - last_stop_time.minute + diff)
             else:
                 last_stop_time = datetime.time(last_stop_time.hour + 0, last_stop_time.minute + diff)
-            print(last_stop_time)
             time_list_2.append(last_stop_time)
         time_list.append(time_list_2)
-    print(time_list)
 
     context = {'routes': routes, 'details': details, 'time_list': time_list, 'starting_hours': starting_hours}
-    return render(request, 'timetable/dashboard.html', context)
+    return render(request, 'timetable/timetables.html', context)
