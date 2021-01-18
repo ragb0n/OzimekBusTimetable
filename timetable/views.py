60a975b33f811ff598875520e14cd583eb10b27a
ragb0n <74256820+ragb0n@users.noreply.github.com>
2021-01-18 19:47:32 +0100

    - added departure time for each stop

--- timetable/views.py
+++ timetable/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 # Create your views here.
 
 from .models import *
@@ -7,7 +8,23 @@ from .models import *
 def home(request):
     routes = Route.objects.all()
     details = Route_details.objects.filter(route_id=1)
-    hours = Schedule.objects.filter(route_id = 1)
-    context = {'routes': routes, 'details': details, 'hours': hours}
+    time_diffs = details.values_list("time_diff", flat=True)
+    starting_hours = Schedule.objects.values_list('start_time', flat=True).filter(route_id = 1)
 
+    time_list = []
+    counter = 0
+    for hour in starting_hours:
+        last_stop_time = hour
+        time_list_2 = []
+        for diff in time_diffs:
+            if last_stop_time.minute + diff > 59:
+                last_stop_time = datetime.time(last_stop_time.hour + 1, last_stop_time.minute - last_stop_time.minute + diff)
+            else:
+                last_stop_time = datetime.time(last_stop_time.hour + 0, last_stop_time.minute + diff)
+            print(last_stop_time)
+            time_list_2.append(last_stop_time)
+        time_list.append(time_list_2)
+    print(time_list)
+
+    context = {'routes': routes, 'details': details, 'time_list': time_list, 'starting_hours': starting_hours}
     return render(request, 'timetable/dashboard.html', context)
